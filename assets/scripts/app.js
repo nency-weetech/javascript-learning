bf81cd59648997e2d46e0eb35a1bc3a38df6de01
nency rana <nency@weetechsolution.com>
2026-06-23 13:00:44 +0530

    strong attack function is created..

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -22,4 +23,21 @@ function attackHandler() {
     alert('You have a draw!');
   }
 }
+
+function strongAttackHandler() {
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("you won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("you lost");
+  } else if(currentMonsterHealth <=0 && currentPlayerHealth <=0) {
+    alert('You have a draw!');
+  }
+}
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler)
